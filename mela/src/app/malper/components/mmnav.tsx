af11f1659d9513486820606efcd117056970bccd
mmrxistin <mmrxistin@gmail.com>
2025-06-26 16:58:32 +0300

    Update mmnav.tsx

--- mela/src/app/malper/components/mmnav.tsx
+++ mela/src/app/malper/components/mmnav.tsx
@@ -15,7 +15,7 @@ function Mmmnavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/malper">Yekazad Software Center</Navbar.Brand>
+        <Navbar.Brand href="/malper">Kurdî War</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
