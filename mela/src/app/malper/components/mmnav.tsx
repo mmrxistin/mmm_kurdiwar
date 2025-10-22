9e07cc9ccd9f99c616deaf3d405fd4ae3ba1e4e2
islamtunc <islamtunc748@gmail.com>
2025-10-22 16:40:55 +0100

    elhamdulillah

--- mela/src/app/malper/components/mmnav.tsx
+++ mela/src/app/malper/components/mmnav.tsx
@@ -16,7 +16,7 @@ function Mmmnavbar() {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="/malper">Günlerin Dili</Navbar.Brand>
-        <Navbar.Brand href="/malper">SîmûrgBaz</Navbar.Brand>
+        <Navbar.Brand href="/malper"></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
