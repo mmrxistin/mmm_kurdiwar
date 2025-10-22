6136b678d492d5dd95885a0c53620b298c2840b3
islamtunc <islamtunc748@gmail.com>
2025-10-22 16:59:20 +0100

    suphanAllah ve Bihamdihi

--- mela/src/app/malper/components/mmbingeh.tsx
+++ mela/src/app/malper/components/mmbingeh.tsx
@@ -5,70 +5,69 @@
 // La ilahe illallah 
 // Allahu Ekber, Allahu Ekber, Allahu Ekber, La ilahe illallah
 // Bila Allah Azze ve Celle me ji sunneta Resulullah Muhammed (s.a.v) neqetine, amin rabbal alemin 
-// Xeyni Allah tu Xweda tune
+// Xeyni Allah tu Xweda tune u Hzreti Muhammed (s.a.v) ji qul u resule Allah e.
 import React from "react";
 
 const Footer = () => (
   <footer className="bg-dark text-white pt-4">
     <div className="container text-center text-md-left">
       <div className="row">
-        {/* About Section */}
+        {/* Hakkımızda */}
         <div className="col-md-4 mt-md-0 mt-3">
-          <h5 className="text-uppercase font-weight-bold">About Us</h5>
+          <h5 className="text-uppercase font-weight-bold">Hakkımızda</h5>
           <p>
-            We are committed to providing the best services and resources for
-            our users. Stay connected with us for updates and news.
+            Kullanıcılarımıza en iyi hizmetleri ve kaynakları sunmayı hedefliyoruz. Güncellemeler ve haberler için bizimle iletişimde kalın.
           </p>
         </div>
 
-        {/* Links Section */}
+        {/* Hızlı Linkler */}
         <div className="col-md-4 mb-md-0 mb-3">
-          <h5 className="text-uppercase font-weight-bold">Quick Links</h5>
+          <h5 className="text-uppercase font-weight-bold">Hızlı Linkler</h5>
           <ul className="list-unstyled">
             <li>
-              <a href="#!" className="text-white">
-                Home
+              <a href="/malper" className="text-white">
+                Anasayfa
               </a>
             </li>
             <li>
-              <a href="#!" className="text-white">
-                About
+              <a href="/malper/firotana-teqwima" className="text-white">
+                Hakkında
               </a>
             </li>
             <li>
-              <a href="#!" className="text-white">
-                Services
+              <a href="/malper/hizmetler" className="text-white">
+                Hizmetler
               </a>
             </li>
             <li>
               <a href="/login" className="text-white">
-                Administration
+                Yönetim
               </a>
             </li>
           </ul>
         </div>
 
-        {/* Social Media Section */}
+        {/* Sosyal Medya */}
         <div className="col-md-4 mb-md-0 mb-3">
-          <h5 className="text-uppercase font-weight-bold">Follow Us</h5>
+          <h5 className="text-uppercase font-weight-bold">Bizi Takip Edin</h5>
           <ul className="list-unstyled d-flex justify-content-center">
             <li className="mx-2">
-              <a href="https://facebook.com" className="text-white">
+              <a href="https://facebook.com" className="text-white" aria-label="Facebook">
                 <i className="fab fa-facebook-f"></i>
               </a>
             </li>
             <li className="mx-2">
-              <a href="https://twitter.com" className="text-white">
+              <a href="https://twitter.com" className="text-white" aria-label="Twitter">
                 <i className="fab fa-twitter"></i>
               </a>
             </li>
             <li className="mx-2">
-              <a href="https://instagram.com" className="text-white">
+              <a href="https://instagram.com" className="text-white" aria-label="Instagram">
                 <i className="fab fa-instagram"></i>
               </a>
             </li>
             <li className="mx-2">
-              <a href="https://linkedin.com" className="text-white">
+              <a href="https://linkedin.com" className="text-white" aria-label="LinkedIn">
                 <i className="fab fa-linkedin-in"></i>
               </a>
             </li>
@@ -78,7 +77,7 @@ const Footer = () => (
     </div>
 
     <div className="text-center py-3">
-      © {new Date().getFullYear()} Copyright:{" "}
+      © {new Date().getFullYear()} Tüm hakları saklıdır:{" "}
       <a href="https://yekazad.com" className="text-white">
         Yekazad
       </a>
